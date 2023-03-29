38189b3ef15d28c1f0c4381c0d60ef59b0723de3
CJ0423 <b09785157888@gmail.com>
2023-03-30 00:04:41 +0800

    修正完成

--- 家教教材/健瑋 copy/圖片移動/123.js
+++ 家教教材/健瑋 copy/圖片移動/123.js
@@ -6,10 +6,12 @@ let pic = [
     "./pic/puppy.jpg",
     "./pic/wood.jpg",
 ]
-let x = 1;
+let x = 0;
 setInterval(function () {
+    console.log(`現在是第${x}張圖片`)//給可用來測試的喔
     inner.style.backgroundImage = "url(" + pic[x] + ")";
     x += 1;
+  
     if (x > 3) {
         x = 0;
     }
