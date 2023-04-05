13cd7f312990fc3a174728e0da30384dd45f7148
CJ0423 <b09785157888@gmail.com>
2023-04-05 15:00:49 +0800

    3

--- 家教教材/健瑋 copy/hong-yue/hong-yue.js
+++ 家教教材/健瑋 copy/hong-yue/hong-yue.js
@@ -10,36 +10,48 @@ let houseTextAbout = document.querySelector("#house-text-about");
 let changeText = document.querySelectorAll('.change-text');
 let changeTextEng = ['Hong-Yue', 'About Us'];
 // <let changeTextCh = ['宏悅家具', '關<br>於<br>我<br>們'];>
-let changeTextCh = ['宏悅家具', '關於我們'];
+let changeTextCh = ['宏悅家具', '關<br>於<br>我<br>們<br>'];
 
 for (let i = 0; i < changeText.length; i++) {
     changeText[i].addEventListener("click", function () {
         if (changeText[i].innerHTML == changeTextCh[i]) {
-            changeText[i].innerHTML = changeTextEng[i];
+            changeText[i].innerHTML = `${changeTextEng[i]}`;
         } else {
             changeText[i].innerHTML = changeTextCh[i];
         }
-
     })
 }
-houseText.addEventListener("mouseover", function () {
-    houseText.innerHTML = "首頁";
-})
+
+let text=[houseText,houseTextQuestion,houseTextAbout]
+let textHover=["首頁","FAQ","關於我們"]
+
+for(let i=0;i<3;i++){
+    text[i].addEventListener("mouseover", function () {
+    text[i].innerHTML=textHover[i]
+    })
+}
+// houseText.addEventListener("mouseover", function () {
+//     houseText.innerHTML = "首頁";
+// })
 houseText.addEventListener("mouseleave", function () {
     houseText.innerHTML = '<img src="./pic/house.svg" alt="" id="house-img-home">';
 })
-houseTextQuestion.addEventListener("mouseover", function () {
-    houseTextQuestion.innerHTML = "FAQ";
-})
-houseTextAbout.addEventListener("mouseover", function () {
-    houseTextAbout.innerHTML = "關於我們";
-})
-houseTextAbout.addEventListener("mouseleave", function () {
-    houseTextAbout.innerHTML = '<img src="./pic/address.svg" alt="" id="house-img-about">';
-})
+
+// houseTextQuestion.addEventListener("mouseover", function () {
+//     houseTextQuestion.innerHTML = "FAQ";
+// })
+
 houseTextQuestion.addEventListener("mouseleave", function () {
     houseTextQuestion.innerHTML = '<img src="./pic/question.svg" alt="" id="house-img-question">';
 })
+
+// houseTextAbout.addEventListener("mouseover", function () {
+//     houseTextAbout.innerHTML = "關於我們";
+// })
+houseTextAbout.addEventListener("mouseleave", function () {
+    houseTextAbout.innerHTML = '<img src="./pic/address.svg" alt="" id="house-img-about">';
+})
+
 
 talkAboutUs.addEventListener("mouseover", function () {
     textAboutUs.style.display = "flex";
