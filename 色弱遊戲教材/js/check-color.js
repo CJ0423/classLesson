2c6cdb23f7ebc6b95e61909727d78ebe8ad9f8af
CJ0423 <b09785157888@gmail.com>
2023-04-16 13:24:49 +0800

    ˇ

--- 色弱遊戲教材/js/check-color.js
+++ 色弱遊戲教材/js/check-color.js
@@ -1,10 +1,12 @@
 function getDom(dom, all) {
-    if (all == 2) { return document.querySelectorAll(dom) }
+    if (all == 2) { return Array.from(document.querySelectorAll(dom))}
+    
     return document.querySelector(dom)
 }
+
 let level = 0
-howMuchBoxWidth = [4, 9, 16, 25, 36, 49, 64, 81, 100, 121]
-howMuchBox = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+let howMuchBoxWidth = [4, 9, 16, 25, 36, 49, 64, 81, 100, 121]
+let howMuchBox = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 let box = getDom(".box")
 let startBtn=getDom(".start-btn")
 let time=getDom(".time")
@@ -21,15 +23,14 @@ return false
 }
 function startGame() {
     if(isOver()){
-        clearInterval(theTime)
+        clearInterval(theTime)//不要在倒數了。
         timeNumber=60;
-        level=0
         startBtn.style.display="block"
         console.log("你輸了")
+        return
     }
     if (isWin()) {
         startBtn.style.display="block"
-        level=0
         timeNumber=60;
         console.log("恭喜過關")
         return
@@ -45,12 +46,11 @@ function startGame() {
     update()
     addSomething()
 }
-
 function isWin() {
     if (level >= 25) {
+        level=0
         clearInterval(theTime)
         return true
-
     }
     else {
         return false
@@ -59,17 +59,15 @@ function isWin() {
 function update() {
     let i = Math.round(level / 3)
     specal = Math.round(Math.random() * (howMuchBoxWidth[i]-1))
-
     box.innerHTML = ``
-
+    console.log(i)
     console.log(specal)
-    let BoxBig = box.clientWidth
     let r=Math.floor(Math.random()*235)
     let g=Math.floor(Math.random()*235)
     let b=Math.floor(Math.random()*235)
 
     let color= ` background-color:rgb(${r},${g},${b});`
-    let specialColor= ` background-color:rgb(${r+50},${g+10},${b+10});`
+    let specialColor= ` background-color:rgb(${r+10},${g+10},${b+10});`
     for (let j = 0; j < howMuchBoxWidth[i]; j++) {
         if(j==specal){
             console.log("有進來嗎")
@@ -82,16 +80,14 @@ function update() {
         }
     }
 }
-
 function timer(){
    theTime= setInterval(()=>{
         timeNumber--
         time.textContent=`剩下時間：${timeNumber}秒`    },1000)
 }
-
 function  addSomething(){
     let pickItem = getDom(".pick-item", 2)
-    pickItem=Array.from(pickItem)
+    // pickItem=Array.from(pickItem)
 
     pickItem.forEach((element,index) => {
         if(index==specal){
