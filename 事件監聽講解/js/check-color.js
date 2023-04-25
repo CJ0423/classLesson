0e8e6b33af3a1602504dd4c817743321d79754a4
CJ0423 <b09785157888@gmail.com>
2023-04-25 14:36:11 +0800

    ˇ

---
+++ 事件監聽講解/js/check-color.js
@@ -0,0 +1,55 @@
+function getDom(dom){
+    return document.querySelector(dom)
+}
+
+const test=getDom(".test")
+const test1=getDom(".test1")
+const test3=getDom(".test3")
+const test4=getDom(".test4")
+const testBoss=getDom(".test-boss")
+
+test.addEventListener("click",testFunction)
+test1.addEventListener("click",testFunction1)
+test3.addEventListener("click",testFunction3)
+test4.addEventListener("click",testFunction4)
+testBoss.addEventListener("click",testBossFunction)
+
+function testFunction(e){
+    // 大部分情況都是如此
+    console.log("這是0產生的") 
+   console.log(this) 
+   console.log(this.textContent) 
+   console.log(e.target)
+}
+function testFunction1(e){
+        // 大部分情況都是如此
+
+    console.log("這是1產生的") 
+   console.log(this) 
+   console.log(e.target)
+   console.log(e.target.value)
+
+}
+function testFunction3(e){
+            // 因為事件冒泡產生的不同結果
+
+    console.log("這是3產生的") 
+   console.log(this) 
+   console.log( e.target) //追蹤到真正被點擊的
+   console.log(e.target.value)
+
+}
+function testBossFunction(e){
+    console.log("這是boss產生的") 
+
+   console.log(this) 
+   console.log(e.target)
+   console.log(e.target.value)
+}
+function testFunction4(e){
+    console.log("這是4產生的") 
+
+   console.log(this) 
+   console.log(e.target)
+   console.log(e.target.value)
+}
