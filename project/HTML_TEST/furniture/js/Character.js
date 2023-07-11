cc2fb4a35733925248257c5066e3f28beb871439
a0956154555 <wayne870726@yahoo.com.tw>
2023-07-11 09:16:24 +0800

    1

--- project/HTML_TEST/furniture/js/Character.js
+++ project/HTML_TEST/furniture/js/Character.js
@@ -35,11 +35,15 @@ let introduceMid2 = document.querySelector(".introduce-mid2");
 let bannerTop = document.querySelector(".banner-top");
 let viewMore = document.querySelector(".view-more");
 let viewMore2 = document.querySelector(".view-more2");
+let viewMore3 = document.querySelector(".view-more3");
+
 let janeClose = document.querySelector(".jane-close");
 let justinClose = document.querySelector(".justin-close");
+let nelsonClose = document.querySelector(".nelson-close");
 
 let janeIntroduce = document.querySelector(".jane-introduce");
 let justinIntroduce = document.querySelector(".justin-introduce");
+let nelsonIntroduce = document.querySelector(".nelson-introduce");
 
 let close1 = document.querySelectorAll(".close1");
 let startAll = document.querySelectorAll(".start-all");
@@ -54,13 +58,21 @@ let janePic1 = document.querySelector(".jane-pic1");
 let janePic2 = document.querySelector(".jane-pic2");
 let janePic3 = document.querySelector(".jane-pic3");
 let janePic4 = document.querySelector(".jane-pic4");
+// ---------------------
 let justinPic1 = document.querySelector(".justin-pic1");
 let justinPic2 = document.querySelector(".justin-pic2");
 let justinPic3 = document.querySelector(".justin-pic3");
 let justinPic4 = document.querySelector(".justin-pic4");
 let justinBtn1 = document.querySelector(".justin-btn1");
 let justinBtn2 = document.querySelector(".justin-btn2");
-
+// ----------------------------------
+let nelsonPic1 = document.querySelector(".nelson-pic1");
+let nelsonPic2 = document.querySelector(".nelson-pic2");
+let nelsonPic3 = document.querySelector(".nelson-pic3");
+let nelsonPic4 = document.querySelector(".nelson-pic4");
+let nelsonBtn1 = document.querySelector(".nelson-btn1");
+let nelsonBtn2 = document.querySelector(".nelson-btn2");
+// ------------------------------------
 topCase.addEventListener("click", function () {
   underCaseTrueOrFalse = !underCaseTrueOrFalse;
   if (underCaseTrueOrFalse) {
@@ -71,6 +83,9 @@ topCase.addEventListener("click", function () {
 });
 let header = document.querySelector("header");
 // --------------------------------------------
+viewMore3.addEventListener("click", function () {
+  nelsonIntroduce.style.display = "flex";
+});
 viewMore2.addEventListener("click", function () {
   justinIntroduce.style.display = "flex";
 });
@@ -83,6 +98,31 @@ janeClose.addEventListener("click", function () {
 justinClose.addEventListener("click", function () {
   justinIntroduce.style.display = "none";
 });
+nelsonClose.addEventListener("click", function () {
+  nelsonIntroduce.style.display = "none";
+});
+// ----------------------------
+nelsonBtn1.addEventListener("click", function () {
+  nelsonPic1.style.display = "flex";
+  nelsonPic2.style.display = "flex";
+  nelsonPic3.style.display = "none";
+  nelsonPic4.style.display = "none";
+  nelsonBtn1.style.color = "black";
+  nelsonBtn1.style.backgroundColor = "white";
+  nelsonBtn2.style.color = "white";
+  nelsonBtn2.style.backgroundColor = "black";
+});
+nelsonBtn2.addEventListener("click", function () {
+  nelsonPic1.style.display = "none";
+  nelsonPic2.style.display = "none";
+  nelsonPic3.style.display = "flex";
+  nelsonPic4.style.display = "flex";
+  nelsonBtn1.style.color = "white";
+  nelsonBtn1.style.backgroundColor = "black";
+  nelsonBtn2.style.color = "black";
+  nelsonBtn2.style.backgroundColor = "white";
+});
+// ----------------------------
 justinBtn1.addEventListener("click", function () {
   justinPic1.style.display = "flex";
   justinPic2.style.display = "flex";
@@ -103,6 +143,7 @@ justinBtn2.addEventListener("click", function () {
   justinBtn2.style.color = "black";
   justinBtn2.style.backgroundColor = "white";
 });
+// --------------------------
 janeBtn1.addEventListener("click", function () {
   janePic1.style.display = "flex";
   janePic2.style.display = "flex";
