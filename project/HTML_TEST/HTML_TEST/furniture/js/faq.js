59b127d637c5371dc08d7126b46e33f94bd3cae2
a0956154555 <wayne870726@yahoo.com.tw>
2023-07-16 19:35:50 +0800

    1

--- project/HTML_TEST/HTML_TEST/furniture/js/faq.js
+++ project/HTML_TEST/HTML_TEST/furniture/js/faq.js
@@ -48,6 +48,20 @@ let otherQuestionRunArr = [
   "輸",
   "入",
 ];
+// -----------------------
+let topCase = document.querySelector(".top-case");
+let underCase = document.querySelector(".under-case");
+let underCaseTrueOrFalse = false;
+topCase.addEventListener("click", function () {
+  underCaseTrueOrFalse = !underCaseTrueOrFalse;
+  // alert(underCaseTrueOrFalse);
+  if (underCaseTrueOrFalse) {
+    underCase.style.display = "flex";
+  } else {
+    underCase.style.display = "none";
+  }
+});
+// -----------------------
 window.addEventListener("load", function () {
   setInterval(() => {
     if (faqChineseNum < faqChineseArr.length) {
