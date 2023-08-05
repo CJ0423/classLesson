ce66217a4b6a84a02cf1fe87d2b304dcbbb3ad17
CJ <b09785157888@gamil.com>
2023-08-05 15:25:34 +0800

    ˇ

---
+++ 後端一點點中的一點點/sql教學/use-sul/html/index2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  class product
+  {
+    public $productImgLink;
+    public $productImgAlt;
+
+    public function __construct($Link, $productImgAlt)
+    {
+      $this->productImgLink = $Link; //裡面的不要有錢錢符號 很重要要記得！！
+      $this->productImgAlt = $productImgAlt;
+    }
+  };
+  $imgSrc = null;
+
+  if (empty($_GET["img-src"])) {
+    echo "沒有商品";
+  } else {
+    // echo "不是空的";
+    $imgSrc = $_GET["img-src"];
+
+    if ($imgSrc === "吹風機") {
+      $appearProduct = new product("../img/1.jpeg", $imgSrc);
+    } else if ($imgSrc === "炒菜鍋") {
+      $appearProduct = new product("../img/2.jpeg", $imgSrc);
+    } else {
+      echo "沒有商品";
+    }
+    echo  "<img src='{$appearProduct->productImgLink}' alt='{$appearProduct->productImgAlt}'>";
+  }
+
+  ?>
+</body>
+<?php
+// echo $imgSrc;
+?>
+
+</html>
